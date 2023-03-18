8c6c35622f7ee5c9925d5631421f6ce2dde5b10d
Muhammad Talha <talhahanfsd@gmail.com>
2023-03-18 20:49:38 +0500

    Design Welcome Section

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import bar from "./../../assets/bar.svg";
+import logo from "./../../assets/logo.svg";
+
+
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <div className='w-[80%] md:w-[80%] mx-auto text-center font-open text-navy'>
+      <img src={bar} alt="" className='py-12 md:py-14 w-full'/>
+      <h2 className="text-dark font-prata text-[40px] md:text-[44px] lg:text-[48px]">
+      Welcome to the <span className="text-blue">wall of</span>
+      </h2>
+      <img src={logo} alt="Alexandria" className='w-full md:w-[60%] lg:h-20 lg:w-auto mx-auto' />
+      <p className="text-[24px] md:text-[28px] lg:text-[32px] max-w-[550px] mx-auto pt-8">This is where we tell you at all our hopes and aspirations.</p>
+      <p className="text-[24px] md:text-[28px] lg:text-[32px]">Behold the wanderer of the literary metaverse.</p>
+      <img src={bar} alt="" className='py-12 md:py-14 w-full'/>
+
+    </div>
   )
 }
 
