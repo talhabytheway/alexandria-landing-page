e3aa590b9db7d90fca9b404bcf1b1a1273f443d6
Muhammad Talha <talhahanfsd@gmail.com>
2023-03-27 21:38:05 +0500

    Mudassars QAs 27/03/23

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,8 +5,8 @@ import star from '../../assets/starHero.svg'
 import send from '../../assets/send.svg'
 const Hero = () => {
   return (
-    <div className="hero pt-20 md:pt-24 lg:pt-28 flex flex-wrap flex-col-reverse lg:flex-row w-[100vw] justify-between lg:items-center xl:mx-auto px-[1.5rem] md:px-[3rem] lg:pr-0 lg:pl-[6rem] lg:max-w-[1500px] font-open">
-      <div className="w-[100%] lg:w-[45%] relative pt-20 lg:pt-0">
+    <div className="hero pt-20 md:pt-24 lg:pt-28 flex flex-wrap flex-col-reverse lg:flex-row w-[100vw] justify-between lg:items-center xl:mx-auto sm:px-[1.5rem] md:px-[3rem] lg:pr-0 lg:pl-[6rem] lg:max-w-[1500px] font-open">
+      <div className="w-[100%] lg:w-[45%] relative pt-20 lg:pt-0 px-[1.5rem] sm:px-0">
         <h1 className="text-[40px] md:text-[64px] lg:text-[96px] text-white font-prata textshad">Your books on<span className="hidden lg:inline">-</span><span className="lg:hidden"> </span>chain</h1>
         <p className="text-white text-[20px] md:text-[26px] lg:text-[32px] drop-shadow-md">The storefront you need to publish and sell books as NFTs</p>
         <div className="flex flex-col lg:flex-row gap-3 md:gap-5 pt-6 [&>*]:lg:inline-block [&>*]:block">
@@ -17,9 +17,9 @@ const Hero = () => {
             PUBLISH
           </button>
         </div>
-        <img src={star} alt="" className="absolute top-[-2%] right-[-2%]"/>
+        <img src={star} alt="" className="absolute top-[-2%] right-[5%] sm:right-[-2%]"/>
       </div>
-        <img src={hero} alt="" className="h-fit ml-auto md:mx-auto lg:mr-0 w-[100%] lg:w-[45%] max-w-[400px] md:max-w-[600px] lg:max-w-[700px]"/>
+        <img src={hero} alt="" className="h-fit ml-auto overflow-hidden mx-auto lg:mr-0 w-[100%] lg:w-[45%] max-w-[400px] md:max-w-[600px] lg:max-w-[700px]"/>
     </div>
   );
 };
--- src/components/Publish/Publish.jsx
+++ src/components/Publish/Publish.jsx
@@ -6,23 +6,23 @@ import book from "../../assets/book.png";
 const Publish = () => {
   return (
     <div className="mt-40 mb-20 w-full mx-auto px-[1.5rem] md:px-[3rem] lg:px-[6rem] lg:max-w-[1500px]">
-      <div className="flex flex-wrap !justify-between pb-6 pt-[55%] md:pt-[350px] lg:py-24 px-4 md:px-8 lg:px-12 bg-purp rounded-2xl leading-relaxed relative">
+      <div className="flex flex-wrap !justify-between pb-12 pt-[70%] sm:pt-[65%] md:pt-[350px] lg:py-24 px-4 md:px-8 lg:px-12 bg-purp rounded-2xl leading-relaxed relative">
         <div className="w-[100%] lg:w-[40%] lg:ml-0 xl:mx-auto">
           <img
             src={book}
             alt=""
-            className=" absolute top-[-40%] sm:top-[-20%] md:top-[-20%] lg:top-auto lg:bottom-[35%] backdrop-blur-[13px] bg-white bg-opacity-[15%] rounded-2xl max-w-[80%] md:max-w-[60%] lg:max-w-[40%] xl:max-w-[60%] mx-auto left-0 right-0 lg:left-auto lg:right-auto lg:mx-0"
+            className=" absolute top-[-20%] sm:top-[-25%] lg:top-auto lg:bottom-[35%] backdrop-blur-[13px] bg-white bg-opacity-[15%] rounded-2xl max-w-[80%] md:max-w-[60%] lg:max-w-[40%] xl:max-w-[60%] mx-auto left-0 right-0 lg:left-auto lg:right-auto lg:mx-0"
           />
         </div>
         <div className="text-white w-full lg:w-[50%]  flex flex-col gap-2 lg:gap-6">
           <h3 className="font-prata text-[40px] md:text-[44px] lg:text-[48px]">
             Publish a book NFT
           </h3>
-          <p className="font-open text-[20px] md:text-[22px] lg:text-[24px]">
+          <p className="text-[#F6F6F6] font-thin font-open text-[20px] md:text-[22px] lg:text-[24px]">
             Duis non orci justo. Nulla lobortis, elit ac imperdiet mollis, nisi
             ante blandit ligula, vel elementum urna velit eu lorem
           </p>
-          <button className=" text-white w-fit font-medium px-10 py-3 text-[20px] rounded-md hover:cursor-pointer hover:duration-500 bg-gradient-to-r from-blue to-pink hover:bg-gradient-to-tr">
+          <button className=" text-white w-fit font-medium px-6 md:px-10 py-3 text-[16px] md:text-[20px] rounded-md hover:cursor-pointer hover:duration-500 bg-gradient-to-r from-blue to-pink hover:bg-gradient-to-tr">
             {" "}
             PUBLISH WORKS{" "}
             <img src={arrow} alt="" className="inline-block pl-2" />
